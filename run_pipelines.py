f06d40dbff19ed3714da37192bb3f5e0182592f3
unknown <SloughJE@gmail.com>
2023-08-23 09:38:58 -0400

    compliance

--- run_pipelines.py
+++ run_pipelines.py
@@ -1,47 +1,34 @@
-import sys 
-#import yaml
 import argparse
-from dotenv import load_dotenv
 import os
+import sys
+from dotenv import load_dotenv
+
 from src.podcast_backend import save_podcast_output
 
 if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser()
-
+    parser = argparse.ArgumentParser(description="Podcast processing script")
+    
     parser.add_argument(
-        "--run_backend",    
-        help="process podcast and save output",
+        "--run_backend",
+        help="Process podcast and save output",
         action="store_true"
     )
-     
-    parser.add_argument(
-        "--make_features",
-        help="make features",
-        action="store_true"
-    )
-    # etc
 
     args = parser.parse_args()
 
     if len(sys.argv) == 1:
-        print("No arguments, please add arguments")
+        print("No arguments provided. Please add arguments.")
     else:
-        #with open("params.yaml") as f:
-        #    params = yaml.safe_load(f)
-
         load_dotenv()
-        print("loading OpenAI API key")
+        print("Loading OpenAI API key...")
         openai_api_key = os.environ.get("OPENAI_API_KEY")
 
         if args.run_backend:
-            rss_url = "https://feeds.simplecast.com/dHoohVNH"
+            rss_url = "https://tenminutepodcast.libsyn.com/rss"
             local_path = 'podcasts/'
-            save_podcast_output(
-                rss_url,
-                local_path,
-                openai_api_key
-            )
+            save_podcast_output(rss_url, local_path, openai_api_key)
+
 
 # rss_url = "https://tenminutepodcast.libsyn.com/rss"
 # local_path = '/content/drive/My Drive/streamlit/'
